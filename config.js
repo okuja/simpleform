da3cccbcaea506f83f5039ea786846a04b35b8ce
Okuja <ajokuja@nwt.ug>
2019-10-02 14:26:06 +0300

    Added a Procfile.

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = defaultValues = {
+    FACEBOOK_GRAPH_API_BASE_URL: 'https://graph.facebook.com/v2.6/',
+}
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+var access_token
+var sender_psid
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,14 +13,64 @@ router.get('/register',function(req,res,next){
   res.render('register');
 })
 
-router.get('/location',function(req,res,next){
-  res.render('location');
+router.get('/location/accesstoken/:access_token/senderpsid/:sender_psid',function(req,res,next){
+ //console.log(req.params);
+
+  access_token = req.params.access_token
+  sender_psid = req.params.sender_psid
+
+  res.render('location');  
 })
 
 
 
-router.post('/register',function(req,res,next){
-  console.log(req.body);
+router.post('/location',function(req,res,next){
+  //console.log(req.body);
+
+  location = {
+    "Latitude":req.body.Latitude,
+    "Longitude":req.body.Longitude
+  }
+
+  // console.log(location)
+  // console.log("Access Token " + access_token)
+  // console.log("Sender Id " + sender_psid)
+
+  callSendAPI(location, access_token, location)
+
+  res.end()
+
 })
+
+
+
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid,access_token ,response) {
+  // Construct the message body
+  console.log('message to be sent: ', response);
+  let request_body = {
+      "recipient": {
+          "id": sender_psid
+      },
+      "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+      "uri": `${config.FACEBOOK_GRAPH_API_BASE_URL}me/messages`,
+      "qs": {
+          "access_token": access_token
+      },
+      "method": "POST",
+      "json": request_body
+  }, (err, res, body) => {
+      if (!err) {
+          console.log('message sent!')
+      } else {
+          console.error("Unable to send message:" + err);
+      }
+  });
+}
 
 module.exports = router;
